1b6e115e0f07ce07bec298bed46abcb58f2442ad
Alexey Vasiliev <leopard.not.a@gmail.com>
2017-12-25 01:48:01 +0200

    gen password

--- webpack/lib/__tests__/mpw.js
+++ webpack/lib/__tests__/mpw.js
@@ -13,42 +13,42 @@ describe('User Max Power with long password', () => {
   })
 
   test('check long password', () => {
-    return mpw.generate('some.important-size.com', 1, null, 'long', MPW.NS)
+    return mpw.generatePassword('some.important-size.com', 1, 'long')
       .then(password => expect(password).toEqual('JidcGoweRowe0#'))
   })
 
   test('check long password with three counter', () => {
-    return mpw.generate('some.important-size.com', 3, null, 'long', MPW.NS)
+    return mpw.generatePassword('some.important-size.com', 3, 'long')
       .then(password => expect(password).toEqual('Yipe7~QiwvJuzu'))
   })
 
   test('check short password', () => {
-    return mpw.generate('some.important-size.com', 1, null, 'short', MPW.NS)
+    return mpw.generatePassword('some.important-size.com', 1, 'short')
       .then(password => expect(password).toEqual('Jid4'))
   })
 
   test('check basic password', () => {
-    return mpw.generate('some.important-size.com', 1, null, 'basic', MPW.NS)
+    return mpw.generatePassword('some.important-size.com', 1, 'basic')
       .then(password => expect(password).toEqual('oiO40uXm'))
   })
 
   test('check medium password', () => {
-    return mpw.generate('some.important-size.com', 1, null, 'medium', MPW.NS)
+    return mpw.generatePassword('some.important-size.com', 1, 'medium')
       .then(password => expect(password).toEqual('Jid4-Lak'))
   })
 
   test('check maximum password', () => {
-    return mpw.generate('some.important-size.com', 1, null, 'maximum', MPW.NS)
+    return mpw.generatePassword('some.important-size.com', 1, 'maximum')
       .then(password => expect(password).toEqual('o7+#6no0URohokcJLA*J'))
   })
 
   test('check name', () => {
-    return mpw.generate('some.important-size.com', 1, null, 'name', MPW.NS)
+    return mpw.generatePassword('some.important-size.com', 1, 'name')
       .then(password => expect(password).toEqual('jidcalake'))
   })
 
   test('check pin', () => {
-    return mpw.generate('some.important-size.com', 1, null, 'pin', MPW.NS)
+    return mpw.generatePassword('some.important-size.com', 1, 'pin')
       .then(password => expect(password).toEqual('6774'))
   })
 })
