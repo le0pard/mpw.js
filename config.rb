85fb5306e398d6518caab7fe823eb3f41a203314
Alexey Vasiliev <leopard.not.a@gmail.com>
2018-01-23 17:59:41 +0200

    Fix cleanup

--- config.rb
+++ config.rb
@@ -29,7 +29,7 @@ assets_dir = ".tmp/dist"
 activate :external_pipeline,
   name: :webpack,
   command: build? ?
-    "rm -fr '#{assets_dir}/*' && NODE_ENV=production ./node_modules/.bin/webpack --bail" :
+    "rm -fr #{assets_dir}/* && NODE_ENV=production ./node_modules/.bin/webpack --bail" :
     './node_modules/.bin/webpack --watch -d --color',
   source: assets_dir,
   latency: 1
